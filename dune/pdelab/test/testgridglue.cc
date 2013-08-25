feb2ce52940a37d93665bc66aa886b4928bfd840
Christian Engwer <christi@dune-project.org>
2013-08-25 10:27:36 +0200

    [test] import test from gridglue ... to be adapted to pdelab

---
+++ dune/pdelab/test/testgridglue.cc
@@ -0,0 +1,136 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include "config.h"
+
+#include <dune/common/version.hh>
+
+#if DUNE_VERSION_NEWER(DUNE_COMMON,2,3)
+#include <dune/common/parallel/mpihelper.hh>
+#else
+#include <dune/common/mpihelper.hh>
+#endif
+#include <iostream>
+
+#include <dune/common/fvector.hh>
+#include <dune/grid/sgrid.hh>
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/geometrygrid.hh>
+#include <dune/geometry/quadraturerules.hh>
+
+#include <dune/grid-glue/extractors/extractorpredicate.hh>
+#include <dune/grid-glue/extractors/codim1extractor.hh>
+
+#include <dune/grid-glue/merging/psurfacemerge.hh>
+#include <dune/grid-glue/adapter/gridglue.hh>
+
+#include <dune/grid-glue/test/couplingtest.hh>
+#include <dune/grid-glue/test/communicationtest.hh>
+
+using namespace Dune;
+
+
+template <class GridView>
+class VerticalFaceDescriptor
+  : public ExtractorPredicate<GridView,1>
+{
+public:
+  VerticalFaceDescriptor(double sliceCoord)
+    : sliceCoord_(sliceCoord)
+  {}
+
+  virtual bool contains(const typename GridView::Traits::template Codim<0>::EntityPointer& eptr,
+                        unsigned int face) const
+  {
+    const int dim = GridView::dimension;
+    const Dune::GenericReferenceElement<double,dim>& refElement = Dune::GenericReferenceElements<double, dim>::general(eptr->type());
+
+    int numVertices = refElement.size(face, 1, dim);
+
+    for (int i=0; i<numVertices; i++)
+      if ( std::abs(eptr->geometry().corner(refElement.subEntity(face,1,i,dim))[0] - sliceCoord_) > 1e-6 )
+        return false;
+
+    return true;
+  }
+
+private:
+  double sliceCoord_;
+};
+
+template <int dim>
+void testNonMatchingCubeGrids()
+{
+
+  // ///////////////////////////////////////
+  //   Make two cube grids
+  // ///////////////////////////////////////
+
+  typedef SGrid<dim,dim> GridType;
+
+  FieldVector<int, dim> elements(2);
+  FieldVector<double,dim> lower(0);
+  FieldVector<double,dim> upper(1);
+
+  GridType cubeGrid0(elements, lower, upper);
+
+  elements = 4;
+  lower[0] += 1;
+  upper[0] += 1;
+
+  GridType cubeGrid1(elements, lower, upper);
+
+
+  // ////////////////////////////////////////
+  //   Set up coupling at their interface
+  // ////////////////////////////////////////
+
+  typedef typename GridType::LevelGridView DomGridView;
+  typedef typename GridType::LevelGridView TarGridView;
+
+  VerticalFaceDescriptor<DomGridView> domdesc(1);
+  VerticalFaceDescriptor<TarGridView> tardesc(1);
+
+  typedef Codim1Extractor<DomGridView> DomExtractor;
+  typedef Codim1Extractor<TarGridView> TarExtractor;
+
+  DomExtractor domEx(cubeGrid0.levelView(0), domdesc);
+  TarExtractor tarEx(cubeGrid1.levelView(0), tardesc);
+
+#if HAVE_PSURFACE
+  typedef PSurfaceMerge<dim-1,dim,double> SurfaceMergeImpl;
+
+  typedef ::GridGlue<DomExtractor,TarExtractor> GlueType;
+
+  SurfaceMergeImpl merger;
+  GlueType glue(domEx, tarEx, &merger);
+
+  glue.build();
+
+  std::cout << "Gluing successful, " << glue.size() << " remote intersections found!" << std::endl;
+  assert(glue.size() > 0);
+
+  // ///////////////////////////////////////////
+  //   Test the coupling
+  // ///////////////////////////////////////////
+
+  testCoupling(glue);
+#else
+    #warning Not testing, because psurface backend is not available.
+#endif
+}
+
+int main(int argc, char *argv[]) try
+{
+  Dune::MPIHelper::instance(argc, argv);
+
+  std::cout << "============================================================\n";
+  testNonMatchingCubeGrids<2>();
+  std::cout << "============================================================\n";
+
+  return 0;
+}
+catch (Exception e) {
+  int i = 0; char** c = 0;
+  std::cout << Dune::MPIHelper::instance(i,c).rank() << ": " << e << std::endl;
+  return 1;
+}
